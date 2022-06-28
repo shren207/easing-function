66bb892cfe5c62af46731c64fa837eb2c67db1cd
Green <shren0812@gmail.com>
2022-06-28 11:59:03 +0900

    Easing.ts return type 명시

--- src/Easing.ts
+++ src/Easing.ts
@@ -1,49 +1,49 @@
 export const easing = {
-  easeLinear(t: number, b: number, c: number, d: number) {
+  easeLinear(t: number, b: number, c: number, d: number): number {
     return (c * t) / d + b;
   },
-  easeInQuad(t: number, b: number, c: number, d: number) {
+  easeInQuad(t: number, b: number, c: number, d: number): number {
     return c * (t /= d) * t + b;
   },
-  easeOutQuad(t: number, b: number, c: number, d: number) {
+  easeOutQuad(t: number, b: number, c: number, d: number): number {
     return -c * (t /= d) * (t - 2) + b;
   },
-  easeInOutQuad(t: number, b: number, c: number, d: number) {
+  easeInOutQuad(t: number, b: number, c: number, d: number): number {
     if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
     return (-c / 2) * (--t * (t - 2) - 1) + b;
   },
-  easeInSine(t: number, b: number, c: number, d: number) {
+  easeInSine(t: number, b: number, c: number, d: number): number {
     return -c * Math.cos((t / d) * (Math.PI / 2)) + c + b;
   },
-  easeOutSine(t: number, b: number, c: number, d: number) {
+  easeOutSine(t: number, b: number, c: number, d: number): number {
     return c * Math.sin((t / d) * (Math.PI / 2)) + b;
   },
-  easeInOutSine(t: number, b: number, c: number, d: number) {
+  easeInOutSine(t: number, b: number, c: number, d: number): number {
     return (-c / 2) * (Math.cos((Math.PI * t) / d) - 1) + b;
   },
-  easeInExpo(t: number, b: number, c: number, d: number) {
+  easeInExpo(t: number, b: number, c: number, d: number): number {
     return t === 0 ? b : c * Math.pow(2, 10 * (t / d - 1)) + b;
   },
-  easeOutExpo(t: number, b: number, c: number, d: number) {
+  easeOutExpo(t: number, b: number, c: number, d: number): number {
     return t === d ? b + c : c * (-Math.pow(2, (-10 * t) / d) + 1) + b;
   },
-  easeInOutExpo(t: number, b: number, c: number, d: number) {
+  easeInOutExpo(t: number, b: number, c: number, d: number): number {
     if (t === 0) return b;
     if (t === d) return b + c;
     if ((t /= d / 2) < 1) return (c / 2) * Math.pow(2, 10 * (t - 1)) + b;
     return (c / 2) * (-Math.pow(2, -10 * --t) + 2) + b;
   },
-  easeInCirc(t: number, b: number, c: number, d: number) {
+  easeInCirc(t: number, b: number, c: number, d: number): number {
     return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
   },
-  easeOutCirc(t: number, b: number, c: number, d: number) {
+  easeOutCirc(t: number, b: number, c: number, d: number): number {
     return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
   },
-  easeInOutCirc(t: number, b: number, c: number, d: number) {
+  easeInOutCirc(t: number, b: number, c: number, d: number): number {
     if ((t /= d / 2) < 1) return (-c / 2) * (Math.sqrt(1 - t * t) - 1) + b;
     return (c / 2) * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
   },
-  easeInElastic(t: number, b: number, c: number, d: number) {
+  easeInElastic(t: number, b: number, c: number, d: number): number {
     let s = 1.70158;
     let p = 0;
     let a = c;
@@ -64,7 +64,7 @@ export const easing = {
       ) + b
     );
   },
-  easeOutElastic(t: number, b: number, c: number, d: number) {
+  easeOutElastic(t: number, b: number, c: number, d: number): number {
     let s = 1.70158;
     let p = 0;
     let a = c;
@@ -83,7 +83,7 @@ export const easing = {
       b
     );
   },
-  easeInOutElastic(t: number, b: number, c: number, d: number) {
+  easeInOutElastic(t: number, b: number, c: number, d: number): number {
     let s = 1.70158;
     let p = 0;
     let a = c;
